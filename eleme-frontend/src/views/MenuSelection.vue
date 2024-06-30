b2939504f210fd8542ff312037bdd9ce74bcd31d
shi <815599298@qq.com>
2024-07-01 00:13:03 +0800

    新建了选择菜品页面，配置了路由
    - 具体内容还没写

---
+++ eleme-frontend/src/views/MenuSelection.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="menu-selection">
+    <h1>Select Dishes</h1>
+    <!-- 菜品列表 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuSelection',
+};
+</script>
+
+<style scoped>
+.menu-selection {
+  /* 样式 */
+}
+</style>
