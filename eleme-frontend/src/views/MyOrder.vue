2949c8202685804fca445e2a3481d229efd8fa66
Rinne3333 <1547488095@qq.com>
2024-07-03 09:54:13 +0800

    22022

---
+++ eleme-frontend/src/views/MyOrder.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-order">
+    <h1>我的订单</h1>
+    <!-- 这里放你的订单内容 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyOrder',
+  // 这里可以添加组件逻辑和数据
+};
+</script>
+
+<style scoped>
+/* 这里可以添加组件样式 */
+</style>
